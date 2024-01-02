72f41d5408ba994befd3903aec4f3676442c2d6a
harinik05 <63025647+harinik05@users.noreply.github.com>
2024-01-02 16:06:07 -0500

    File done and made. Solution doesnt fully make sense to me at the time

---
+++ DP/UniquePaths2_Soln.java
@@ -0,0 +1,42 @@
+class UniquePaths2_Soln {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        // If the starting point or the destination point has an obstacle, there is no path
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        // Create a DP array
+        int[][] dpArr = new int[m][n];
+
+        // Initialize the starting point
+        dpArr[0][0] = 1;
+
+        // Fill the first row
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                dpArr[i][0] = dpArr[i - 1][0];
+            }
+        }
+
+        // Fill the first column
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 0) {
+                dpArr[0][j] = dpArr[0][j - 1];
+            }
+        }
+
+        // Fill the DP array
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dpArr[i][j] = dpArr[i - 1][j] + dpArr[i][j - 1];
+                }
+            }
+        }
+
+        return dpArr[m - 1][n - 1];
+    }
+}
