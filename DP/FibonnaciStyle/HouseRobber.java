631a9111a8a45672daa4f26805072cd59962ca12
harinik05 <63025647+harinik05@users.noreply.github.com>
2024-01-02 11:46:00 -0500

    Create HouseRobber.java

---
+++ DP/FibonnaciStyle/HouseRobber.java
@@ -0,0 +1,24 @@
+class HouseRobber {
+    public int rob(int[] nums) {
+        //1. set up the base case and n 
+        int n = nums.length;
+        if (n== 1){
+            return nums[0];
+        }
+        if (n == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+
+        //2. set up the base values
+        int firstVal = nums[0];
+        int secondVal = Math.max(nums[0], nums[1]);
+        int thirdVal = 0;
+        for(int i = 2;i<n;i++){
+            thirdVal = Math.max(firstVal + nums[i], secondVal);
+            firstVal = secondVal;
+            secondVal = thirdVal;
+        }
+
+        return thirdVal;
+    }
+}
