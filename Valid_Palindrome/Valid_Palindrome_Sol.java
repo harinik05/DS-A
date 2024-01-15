8d144ac833c1bc82bd48d814848195ec4b1bdd69
harinik05 <hka747910@gmail.com>
2024-01-14 20:20:38 -0500

    pushing new code with updates for alphanumeric in

--- Valid_Palindrome/Valid_Palindrome_Sol.java
+++ Valid_Palindrome/Valid_Palindrome_Sol.java
@@ -1,40 +1,38 @@
 package Valid_Palindrome;
 
-public class Valid_Palindrome_Sol {
-
-    //method for finding the palindrome
+class Valid_Palindrome_Sol {
     public String returnPalindrome(String inString){
-        //a. convert to lower case
-        inString = inString.toLowerCase();
 
-        //b. remove all non-alphabetic characters
-        inString = inString.replaceAll("[a-zA-Z0-9]", "");
+       //c. put into a char array
+       char[] charArr = inString.toCharArray();
+       inString = "";
 
-        //c. put into a char array
-        char[] charArr = inString.toCharArray();
-        inString = "";
+       //d. loop through the entire char array and put in a result string
+       for(int i = charArr.length-1;i>=0;i--){
+           inString+=charArr[i];
+       }
 
-        //d. loop through the entire char array and put in a result string
-        for(int i = charArr.length-1;i>=0;i--){
-            inString+=charArr[i];
-        }
+       //e. return the inString
+       return inString;
+   }
 
-        //e. return the inString
-        return inString;
-    }
+   public boolean checkIfPalindrome(String inString){
+       //a. convert to lower case
+       inString = inString.toLowerCase();
 
-    //method 2: check if its a palindrome
-    public boolean checkIfPalindrome(String inString){
-        String palindromeString = returnPalindrome(inString);
-        boolean val = (palindromeString == inString)? true:false;
-        return val;
-    }
+       //b. remove all non-alphabetic characters
+       inString = inString.replaceAll("[^a-zA-Z0-9]", "");
+       String palindromeString = returnPalindrome(inString);
+       boolean val = (palindromeString.equals(inString))? true:false;
+       return val;
+   }
 
-    public static void main(String[] args){
-        //create an object
-        Valid_Palindrome_Sol obj = new Valid_Palindrome_Sol();
-        boolean result = obj.checkIfPalindrome("super");
-        System.out.println(result);
+   public boolean isPalindrome(String s) {
+       boolean result = checkIfPalindrome(s);
+       return result;
+   }
+
+   public static void main(String[] args){
         
-    }
+   }
 }
