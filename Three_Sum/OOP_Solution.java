21345ef3ac494998df2c762decd43313f0c20543
harinik05 <hka747910@gmail.com>
2024-01-15 19:00:12 -0500

    constructor for the method

---
+++ Three_Sum/OOP_Solution.java
@@ -0,0 +1,20 @@
+package Three_Sum;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class OOP_Solution {
+
+    //1. define the corresponding data structures
+    public Set<List<Integer>> results;
+    public Map<Integer, Integer> mainMap;
+
+    //2. Define a constructor
+    public OOP_Solution(Set<List<Integer>> resultsIn, Map<Integer,Integer> mainMapIn){
+        this.results = resultsIn;
+        this.mainMap = mainMapIn;
+    }
+
+    //2. 
+}
