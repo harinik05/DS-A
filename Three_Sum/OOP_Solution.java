aba1719b6de156de5da2d61d9cf0078708b8aa56
harinik05 <hka747910@gmail.com>
2024-01-15 19:42:45 -0500

    logic for modifying thr results

--- Three_Sum/OOP_Solution.java
+++ Three_Sum/OOP_Solution.java
@@ -1,5 +1,6 @@
 package Three_Sum;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,5 +17,33 @@ public class OOP_Solution {
         this.mainMap = mainMapIn;
     }
 
-    //2. 
+    //3. finding the three sum array
+    public void returnListOfStuff(int[] nums){
+        //a. loop through the outer part of the array
+        for(int i = 0;i<nums.length;i++){
+            //b. loop through the inner part of the array
+            for(int j = i+1;j<nums.length;j++){
+                //c. determine the complement
+                int complement = -nums[i]-nums[j];
+                //d. check if its in the hashmap
+                if(mainMap.containsKey(complement) && mainMap.get(complement) == i){
+                    //e. put it as a list (three elements)
+                    Integer[] arr = {nums[i],nums[j],complement};
+                    Arrays.sort(arr);
+                    List<Integer> triplets = Arrays.asList(arr);
+
+                    //f. put in the results
+                    results.add(triplets);
+                }
+
+                //g. put this in the hashmap
+                mainMap.put(nums[j],i);
+            }
+        }
+    }
+
+    //4. main method
+    public static void main(String[] args){
+        
+    }
 }
