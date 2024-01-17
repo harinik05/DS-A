7ab61da28ba0b6bbfcd03721a6788595d3d9d019
harinik05 <hka747910@gmail.com>
2024-01-17 16:34:53 -0500

    finished with the constructor for the hashset

---
+++ Longest_Substring_Without_Repeating_Characters/LongestSubstring.java
@@ -0,0 +1,21 @@
+package Longest_Substring_Without_Repeating_Characters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstring {
+
+    //1. define the data structures
+    private Set<Character> nonDuplicates;
+
+    //2. constructor
+    public LongestSubstring(){
+        this.nonDuplicates = new HashSet<>();
+    }
+
+    //3. find the longest substring implement the method (length())
+
+    public static void main(String[] args){
+
+    }
+}
