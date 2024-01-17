10b1aac3ae2093838333921540739ce918c34e59
harinik05 <hka747910@gmail.com>
2024-01-17 16:56:27 -0500

    defined the ds

--- Longest_Repeating_Character_Replacement/LongestRepeat.java
+++ Longest_Repeating_Character_Replacement/LongestRepeat.java
@@ -1,7 +1,12 @@
 package Longest_Repeating_Character_Replacement;
 
 public class LongestRepeat {
+
+    //1. define the data structure required to store the values
+    private int[] mainStruct;
+
+    //2. define a constructor
     public static void main(String[] args){
-        
+
     }
 }
