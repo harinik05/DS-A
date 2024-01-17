b2c2ca3f066916af23d547f74e221f40fe832182
harinik05 <hka747910@gmail.com>
2024-01-17 16:39:58 -0500

    wrote the function

--- Longest_Substring_Without_Repeating_Characters/LongestSubstring.java
+++ Longest_Substring_Without_Repeating_Characters/LongestSubstring.java
@@ -14,8 +14,33 @@ public class LongestSubstring {
     }
 
     //3. find the longest substring implement the method (length())
+    public int findLongestSubstringLength(String s){
+        //1. initialize the array storing all the chars
+        char[] arr = s.toCharArray();
 
+        //2. initialize the windows
+        int leftWindow = 0;
+        int rightWindow = 0;
+        int output = 0;
+
+        //3. loop through the right window
+        for(;rightWindow<s.length();rightWindow++){
+            //4. skip to adjust the window
+            while(nonDuplicates.contains(arr[rightWindow])){
+                nonDuplicates.remove(arr[leftWindow]);
+                leftWindow++;
+            }
+
+            //5. keep on incrementing the right window
+            nonDuplicates.add(arr[rightWindow]);
+
+            //6. determine the output
+            output = Math.max(output, nonDuplicates.size());
+        }
+
+        return output;
+    }
     public static void main(String[] args){
-
+        
     }
 }
