59391f54ca9db3c80e443cc0a66a3cabc1ca3ae1
harinik05 <hka747910@gmail.com>
2024-01-17 17:39:51 -0500

    function code

--- Valid_Parenthesis/StackSoln.java
+++ Valid_Parenthesis/StackSoln.java
@@ -19,6 +19,29 @@ public class StackSoln {
         this.standardMap.put('}','{');
         this.standardMap.put(']','[');
     }
+
+    //3. define the method to say if this is a valid expression for brackets
+    public boolean determineValidExpression(String s){
+        //1. create an array for storing the characters in the string
+        char[] arr = s.toCharArray();
+
+        //2. loop through this array
+        for(int i=0;i<s.length();i++){
+            //3. check if the closed bracket is there
+            if(this.standardMap.containsKey(arr[i])){
+                //4. retrieve the top elem and compare it with the value
+                char topElem = openBrackets.empty()?'*':openBrackets.pop();
+                if(this.standardMap.get(arr[i])!=topElem){
+                    return false;
+                }
+            }else{
+                openBrackets.add(arr[i]);
+            }
+        }
+
+        //3. everything should be popped off if valid
+        return openBrackets.isEmpty();
+    }
     public static void main(String[] args){
 
     }
