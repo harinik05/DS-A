4b4882b9b1ea39611487c32efea4d72a5a8fab18
harinik05 <hka747910@gmail.com>
2024-01-17 17:03:00 -0500

    wrote the function

--- Longest_Repeating_Character_Replacement/LongestRepeat.java
+++ Longest_Repeating_Character_Replacement/LongestRepeat.java
@@ -10,7 +10,31 @@ public class LongestRepeat {
         this.mainStruct = new int[26];
     }
 
-    //3. 
+    //3. define the sliding window method
+    public int performSlidingWindow(String s, int k){
+        //1. define the windows
+        int leftWindow = 0;
+        int rightWindow = 0;
+        //2. convert s to array
+        char[] charArr = s.toCharArray();
+        int count = 0;
+        int output = 0;
+        //3. loop through the array
+        for(;rightWindow<s.length();rightWindow++){
+            charArr[rightWindow-'A']++;
+            count = Math.max(count, charArr[rightWindow-'A']);
+            while(rightWindow - leftWindow +1 - count > k){
+                //4. adjust the left window
+                charArr[leftWindow-'A']--;
+                leftWindow++;
+            }
+
+            //5. calculate the output
+            output = Math.max(output,rightWindow-leftWindow+1);
+        }
+        //6. return the output
+        return output;
+    }
     public static void main(String[] args){
 
     }
