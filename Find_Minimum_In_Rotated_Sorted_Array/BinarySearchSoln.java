fb505a4cb25727540cf77bc2543ced71342c4f3e
harinik05 <hka747910@gmail.com>
2024-01-17 22:19:35 -0500

    accepted solution for binary search to find the min

--- Find_Minimum_In_Rotated_Sorted_Array/BinarySearchSoln.java
+++ Find_Minimum_In_Rotated_Sorted_Array/BinarySearchSoln.java
@@ -13,9 +13,40 @@ public class BinarySearchSoln {
         this.outputNum = 0;
     }
 
-    //3, 
+    //3. function to return the min
+    public int returnMin(int[] nums){
+        //base case
+        if(nums.length == 0){
+            return -1;
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+        while(leftPointer < rightPointer){
+            //define the midpoint
+            int midpoint = (leftPointer+rightPointer)/2;
+
+            //check if the min is in the midpoint
+            if(midpoint>0 && nums[midpoint] > nums[midpoint-1]){
+                return nums[midpoint];
+            }
+
+            //left sided condition
+            else if(nums[midpoint]>=nums[leftPointer] && nums[midpoint]>nums[rightPointer]){
+                leftPointer = midpoint+1; //focus more on the right side
+            }
+            //right sided condition
+            else{
+                rightPointer = midpoint-1; //focus more on the left side
+            }
+
+
+        }
+        return nums[leftPointer];
+    }
 
     public static void main(String[] args){
-
+        int[] nums = {7,8,9,1,2,5};
+        
     }
 }
