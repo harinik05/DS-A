6f3c69806d01d2256ff3397944ebdf6b7695e84d
harinik05 <hka747910@gmail.com>
2024-01-17 22:03:58 -0500

    variables initialized

--- Find_Minimum_In_Rotated_Sorted_Array/BinarySearchSoln.java
+++ Find_Minimum_In_Rotated_Sorted_Array/BinarySearchSoln.java
@@ -1,5 +1,14 @@
 package Find_Minimum_In_Rotated_Sorted_Array;
 
 public class BinarySearchSoln {
-    
+    //1. initialize the pointers and final result
+    private int leftPointer;
+    private int rightPointer;
+    private int outputNum;
+
+    //2. 
+
+    public static void main(String[] args){
+
+    }
 }
