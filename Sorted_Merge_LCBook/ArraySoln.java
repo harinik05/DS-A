51af3c3132a1b0e2c04ed773fa19c50d14824ab4
harinik05 <hka747910@gmail.com>
2024-01-18 19:07:30 -0500

    constructor done

--- Sorted_Merge_LCBook/ArraySoln.java
+++ Sorted_Merge_LCBook/ArraySoln.java
@@ -1,6 +1,21 @@
 package Sorted_Merge_LCBook;
 
 public class ArraySoln {
+    //1. private member variables
+    private int[] arrayA;
+    private int[] arrayB;
+
+    //2. constructor
+    public ArraySoln(int[] inArrA, int[] inArrB){
+        this.arrayA = inArrA;
+        this.arrayB = inArrB;
+        this.arrayA = new int[inArrA.length + inArrB.length];
+    }
+
+    //3. method to take care of merging both arrays
+    public void mergeTwoArrays(){
+        //a. 
+    }
     public static void main(String[] args){
 
     }
