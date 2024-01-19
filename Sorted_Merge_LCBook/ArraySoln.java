af01359452470d0eb6b522386351b961e2beab7d
harinik05 <hka747910@gmail.com>
2024-01-18 19:21:21 -0500

    determining the lengths of both arrays

--- Sorted_Merge_LCBook/ArraySoln.java
+++ Sorted_Merge_LCBook/ArraySoln.java
@@ -4,17 +4,25 @@ public class ArraySoln {
     //1. private member variables
     private int[] arrayA;
     private int[] arrayB;
+    private int mergedIndx;
+    private int arrALen;
+    private int arrBLen;
 
     //2. constructor
     public ArraySoln(int[] inArrA, int[] inArrB){
         this.arrayA = inArrA;
         this.arrayB = inArrB;
-        this.arrayA = new int[inArrA.length + inArrB.length];
+        this.mergedIndx = this.arrayA.length + this.arrayB.length -1;
+        this.arrALen = inArrA.length;
+        this.arrBLen = inArrB.length;
+       // this.arrayA = new int[inArrA.length + inArrB.length];
     }
 
     //3. method to take care of merging both arrays
     public void mergeTwoArrays(){
-        //a. 
+        //1. start comparing the elements from the end of the array
+        
+
     }
     public static void main(String[] args){
 
