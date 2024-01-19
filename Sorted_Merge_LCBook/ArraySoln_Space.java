7afe7f1b79e402b5f6d0c6c263668ffcb08946b4
harinik05 <hka747910@gmail.com>
2024-01-18 20:09:41 -0500

    variabels initialized

--- Sorted_Merge_LCBook/ArraySoln_Space.java
+++ Sorted_Merge_LCBook/ArraySoln_Space.java
@@ -2,6 +2,12 @@ package Sorted_Merge_LCBook;
 
 public class ArraySoln_Space {
     
+    //1. here are the private arrays that are given to us
+    private int[] array1;
+    private int[] array2;
+
+    //2. constructor
+    
 
     public static void main(String[] args){
 
