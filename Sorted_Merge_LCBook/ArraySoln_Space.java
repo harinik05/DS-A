0ac7ee6b4b3d4bfa695d99eb7f7975dd4770cc83
harinik05 <hka747910@gmail.com>
2024-01-18 20:36:16 -0500

    merge2 arrays

--- Sorted_Merge_LCBook/ArraySoln_Space.java
+++ Sorted_Merge_LCBook/ArraySoln_Space.java
@@ -1,4 +1,4 @@
-package Sorted_Merge_LCBook;
+//package Sorted_Merge_LCBook;
 
 public class ArraySoln_Space {
     
@@ -18,8 +18,49 @@ public class ArraySoln_Space {
         this.arrayFinal = new int[this.len1 + this.len2];
     }
 
-    public static void main(String[] args){
+    //3. merge the arrays together
+    public void merge2Arrays(int[] minArr, int[] maxArr){
+        int i = 0; // pointer for minArr
+        int j = 0; // pointer for maxArr
+        int counter = 0;
 
+        while (i < minArr.length && j < maxArr.length) {
+            if (minArr[i] <= maxArr[j]) {
+                arrayFinal[counter] = minArr[i];
+                i++;
+            } else {
+                arrayFinal[counter] = maxArr[j];
+                j++;
+            }
+            counter++;
+        }
+
+        // Copy the remaining elements from minArr, if any
+        while (i < minArr.length) {
+            arrayFinal[counter] = minArr[i];
+            i++;
+            counter++;
+        }
+
+        // Copy the remaining elements from maxArr, if any
+        while (j < maxArr.length) {
+            arrayFinal[counter] = maxArr[j];
+            j++;
+            counter++;
+        }
+    }
+
+    public static void main(String[] args){
+        int[] nums1 = {1,2,3,4,5};
+        int[] nums2 = {3,4,5,6,7};
+        ArraySoln_Space obj = new ArraySoln_Space(nums1, nums2);
+
+        obj.merge2Arrays(nums1, nums2);
+
+        for(int num: obj.arrayFinal){
+            System.out.println(num);
+        }
+        
     }
     
 }
