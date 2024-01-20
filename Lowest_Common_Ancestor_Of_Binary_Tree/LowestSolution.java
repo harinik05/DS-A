bfae0ca81a1a346389cc610c363b5d66dc67fdc0
harinik05 <hka747910@gmail.com>
2024-01-20 10:38:32 -0500

    lowest common ancestor function - > travel left if everything is small and travel right is everything is right

--- Lowest_Common_Ancestor_Of_Binary_Tree/LowestSolution.java
+++ Lowest_Common_Ancestor_Of_Binary_Tree/LowestSolution.java
@@ -5,13 +5,35 @@ import javax.swing.tree.TreeNode;
 class LowestSolution {
     //1. data structure
     private TreeNode root;
-    
+
     //2. constructor
     public LowestSolution(TreeNode inRoot){
         this.root = inRoot;
     }
+    
+    //3. lowest common ancestor function
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        //a. check base cases
+        TreeNode current = root;
+        if(current == null){
+            return null;
+        }
+
+        //b. check the normal case
+        while(current!=null){
+            //c. move the current to the right if both are greater
+            if(p.data > current.data && q.data > current.data){
+                current = current.right;
+            }
+            //d. move the current to the left if both are smaller
+            else if(p.data < current.data && q.data <current.data){
+                current = current.left;
+            }
+            //e. else condition: return the current
+            else{
+                return current;
+            }
+        }
     }
 
     public static void main(String[] args){
