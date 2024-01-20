e7ebdfaf087e44e3ad9bb176e411e9a70552384f
harinik05 <hka747910@gmail.com>
2024-01-20 18:12:33 -0500

    defined the required data structures

--- Binary_Tree_Level_Order_Traversal/LevelOrder_Soln.java
+++ Binary_Tree_Level_Order_Traversal/LevelOrder_Soln.java
@@ -1,7 +1,16 @@
 package Binary_Tree_Level_Order_Traversal;
 
+import java.util.List;
+import java.util.Queue;
+
 public class LevelOrder_Soln {
+
+    //1. define the required data structures
+    private List<List<Integer>> outputResult;
+    private Queue<TreeNode> queue;
+
+    //2. 
     public static void main(String[] args){
-        
+
     }
 }
