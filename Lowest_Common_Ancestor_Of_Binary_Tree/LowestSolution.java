70d5264c907d70c1edf9444c1da2bd40e9321a06
harinik05 <hka747910@gmail.com>
2024-01-20 10:28:01 -0500

    wrote the skeleton code

---
+++ Lowest_Common_Ancestor_Of_Binary_Tree/LowestSolution.java
@@ -0,0 +1,13 @@
+package Lowest_Common_Ancestor_Of_Binary_Tree;
+
+import javax.swing.tree.TreeNode;
+
+class LowestSolution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
