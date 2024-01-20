39b3cb34b735112f8ef15c422cee10b2c3520bbe
harinik05 <hka747910@gmail.com>
2024-01-20 10:30:43 -0500

    root node established with a constructor

--- Lowest_Common_Ancestor_Of_Binary_Tree/LowestSolution.java
+++ Lowest_Common_Ancestor_Of_Binary_Tree/LowestSolution.java
@@ -3,11 +3,18 @@ package Lowest_Common_Ancestor_Of_Binary_Tree;
 import javax.swing.tree.TreeNode;
 
 class LowestSolution {
+    //1. data structure
+    private TreeNode root;
+    
+    //2. constructor
+    public LowestSolution(TreeNode inRoot){
+        this.root = inRoot;
+    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
     }
 
     public static void main(String[] args){
-        
+
     }
 }
