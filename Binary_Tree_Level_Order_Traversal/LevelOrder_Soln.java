bef9609aac10c04b8748bebe0223c60297e01d32
harinik05 <hka747910@gmail.com>
2024-01-20 18:13:40 -0500

    constructor initiated

--- Binary_Tree_Level_Order_Traversal/LevelOrder_Soln.java
+++ Binary_Tree_Level_Order_Traversal/LevelOrder_Soln.java
@@ -1,7 +1,11 @@
 package Binary_Tree_Level_Order_Traversal;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
 
 public class LevelOrder_Soln {
 
@@ -9,7 +13,13 @@ public class LevelOrder_Soln {
     private List<List<Integer>> outputResult;
     private Queue<TreeNode> queue;
 
-    //2. 
+    //2. constructor
+    public LevelOrder_Soln(){
+        this.outputResult = new ArrayList<List<Integer>>();
+        this.queue = new LinkedList<>();
+    }
+
+    //3. 
     public static void main(String[] args){
 
     }
