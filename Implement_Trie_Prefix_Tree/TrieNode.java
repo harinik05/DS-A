7800f6d53c5ab8beb555837bc0bc2871ab1dfae7
harinik05 <hka747910@gmail.com>
2024-01-24 15:46:23 -0500

    here are the member variables of a node class

---
+++ Implement_Trie_Prefix_Tree/TrieNode.java
@@ -0,0 +1,15 @@
+package Implement_Trie_Prefix_Tree;
+
+import java.util.Map;
+
+public class TrieNode {
+
+    //member variables
+    public Map<Character, TrieNode> childrenMap;
+    public boolean endOfWordFlag;
+
+    //
+    public static void main(String[] args){
+
+    }
+}
