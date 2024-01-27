c5b1929d50d8bad1ff8cc25bfcc2866d90c34769
harinik05 <hka747910@gmail.com>
2024-01-26 19:40:31 -0500

    doesword exist function

---
+++ Word_Search/BackTrackingSoln.java
@@ -0,0 +1,12 @@
+package Word_Search;
+
+public class BackTrackingSoln {
+
+    //define the member variables 
+    private char[][] board;
+    private int ROWSCOUNTER;
+    private int COLUMNSCOUNTER;
+    public static void main(String[] args){
+
+    }
+}
