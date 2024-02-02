5b3318fb2861f5c051e2178ea014477440fa49ce
harinik05 <hka747910@gmail.com>
2024-02-01 19:07:26 -0500

    initiwlized the variables for grid search

--- Number_Of_Islands/Solution.java
+++ Number_Of_Islands/Solution.java
@@ -3,7 +3,12 @@ package Number_Of_Islands;
 public class Solution {
 
     public int numIslands(char[][] grid){
-        
+        //1. initialize the variables
+        int rows = grid.length;
+        int columns = grid[0].length;
+        int counter = 0;
+
+        //2. 
     }
     public static void main(String[] args){
 
