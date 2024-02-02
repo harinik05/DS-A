e847643f4150511e7b6d306d2b06b661dbcea6cd
harinik05 <hka747910@gmail.com>
2024-02-01 19:14:07 -0500

    accepted solution | 2 ms runtime

--- Number_Of_Islands/Solution.java
+++ Number_Of_Islands/Solution.java
@@ -8,7 +8,37 @@ public class Solution {
         int columns = grid[0].length;
         int counter = 0;
 
-        //2. 
+        //2. double for loop 
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<columns;j++){
+                //a. check if 1 is in the grid search
+                if(grid[i][j] == '1'){
+                    counter++;
+                    callbfs(grid, i, j);
+                }
+            }
+        }
+        //3. return the counter
+        return counter;
+    }
+
+    private void callbfs(char[][] grid, int i, int j){
+        //1. check the boundaries and visited status
+        if(i<0 || i>=grid.length|| j<0 || j>= grid[0].length|| grid[i][j] == '0'){
+            return;
+        }
+
+        //2. check the visited
+        grid[i][j] = '0';
+
+        //up
+        callbfs(grid, i+1, j);
+        //down
+        callbfs(grid, i-1, j);
+        //left
+        callbfs(grid, i, j-1);
+        //right
+        callbfs(grid, i, j+1);
     }
     public static void main(String[] args){
 
