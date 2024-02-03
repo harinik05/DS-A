dc6515fbe1883594d5e7df223216ba4a35fd882d
harinik05 <hka747910@gmail.com>
2024-02-03 17:00:59 -0500

    function done and running with accepted resp and runtime of 28 ms

--- Clone_Graph/Clone_Graph.java
+++ Clone_Graph/Clone_Graph.java
@@ -15,7 +15,45 @@ public class Clone_Graph {
         this.hm = HM;
     }
 
-    //2. 
+    //2. create the function
+    public Node cloneGraph(Node node) {
+        /*
+        Using Depth First Search (DFS) as a sol to Deep Copy (node)
+Create a hashmap that stores as original node: duplicate node
+Base case: node = null -> return node
+Check if the node is in the hashmap. If it is, return node
+Else, create a clone node and place it in the hashmap
+Iterate through the neighbors and put in the new cloned ones (recursively add neighbor). Then, returned the cloned node
+
+        */
+
+    
+        //Base case: node = null -> return node
+        if(node==null){
+            return node;
+        }
+
+        //Check if the node is in the hashmap. If it is, return node
+        if(hm.containsKey(node)){
+            return hm.get(node);
+        }
+
+        //Else, create a clone node and place it in the hashmap
+        Node cloneNode = new Node(node.value);
+        hm.put(node,cloneNode);
+
+
+        //Iterate through the neighbors and put in the new cloned ones (recursively add neighbor). Then, returned the cloned node
+        for(Node neighbor: node.neighbors){
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return cloneNode;
+
+
+
+
+    }
 
     public static void main(String[] args){
 
