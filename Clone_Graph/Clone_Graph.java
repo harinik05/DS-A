86c752cce7c1d1718de90d54df890ff1615d9ffe
harinik05 <hka747910@gmail.com>
2024-02-03 16:59:22 -0500

    created the member variables and constructors initialized

--- Clone_Graph/Clone_Graph.java
+++ Clone_Graph/Clone_Graph.java
@@ -1,16 +1,22 @@
 package Clone_Graph;
 
+import java.util.HashMap;
 import java.util.Map;
 
 //approached through the breath first search method
 
 
 public class Clone_Graph {
+    //0. create the member variables - initialized for this purpose
+    private Map<Node, Node> hm;
 
     //1. create a constructor
     public Clone_Graph(Map<Node, Node> HM){
-
+        this.hm = HM;
     }
+
+    //2. 
+
     public static void main(String[] args){
 
     }
