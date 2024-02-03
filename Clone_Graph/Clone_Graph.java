f9290736f44b7dfcf2f2f506932e488b189fdd4b
harinik05 <hka747910@gmail.com>
2024-02-03 16:52:12 -0500

    graph node class created and constructors are being initialized for this

--- Clone_Graph/Clone_Graph.java
+++ Clone_Graph/Clone_Graph.java
@@ -1,10 +1,16 @@
 package Clone_Graph;
 
+import java.util.Map;
+
 //approached through the breath first search method
+
 
 public class Clone_Graph {
 
     //1. create a constructor
+    public Clone_Graph(Map<Node, Node> HM){
+
+    }
     public static void main(String[] args){
 
     }
---
+++ Clone_Graph/Node.java
@@ -0,0 +1,18 @@
+package Clone_Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    //1. define the public member variables
+    public int value;
+    public List<Node> neighbors;
+
+    //2.  constructor (empty)
+    public Node(){
+        this.value = 0;
+        this.neighbors = new ArrayList<>();
+    }
+
+    //2. 
+}
