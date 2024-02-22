00fb93ad97020c04f8c155f5e34a068a9cf1b496
harinik05 <hka747910@gmail.com>
2024-02-22 15:42:14 -0500

    initialized the variables

--- Maximum_Subarray_LC/Solution1.java/src/App.java
+++ Maximum_Subarray_LC/Solution1.java/src/App.java
@@ -1,4 +1,13 @@
 public class App {
+
+    //1. Describe and initialize 
+    private int currentSum;
+    private int totalSum;
+
+    //2. Describe the function
+    public int maxSumArray(int[] nums){
+        
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
