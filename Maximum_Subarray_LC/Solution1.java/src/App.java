8d30bc5df81d8df49b2cf481bc6f3f45c7ca16e7
harinik05 <hka747910@gmail.com>
2024-02-22 16:00:11 -0500

    accepted 1 ms runtime

--- Maximum_Subarray_LC/Solution1.java/src/App.java
+++ Maximum_Subarray_LC/Solution1.java/src/App.java
@@ -6,7 +6,21 @@ public class App {
 
     //2. Describe the function
     public int maxSumArray(int[] nums){
-        
+        //1. initialize the values for these variables 
+        currentSum = 0;
+        totalSum = nums[0];
+
+        //2. for loop to go through all the elements 
+        for(int number: nums){
+            if(currentSum<0){
+                currentSum=0;
+            }
+            currentSum+=number;
+            totalSum = Math.max(currentSum, totalSum);
+
+        }
+
+        return totalSum;
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
