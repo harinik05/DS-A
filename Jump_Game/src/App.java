f40ea5b7255f29970c97a0177f4242849198c5dd
harinik05 <hka747910@gmail.com>
2024-03-12 21:33:01 -0400

    change

--- Jump_Game/src/App.java
+++ Jump_Game/src/App.java
@@ -1,4 +1,19 @@
 public class App {
+
+    //1. initialize the variables
+    private int goalPost;
+
+    //2. define a function for this
+    public boolean canJump(int[] nums){
+        //1. start from the last one 
+        goalPost = nums.length-1;
+
+        //2. start from the last and go through all of them
+        for(int i = nums.length-1;i>0;i--){
+            //a. check if the value at prev is good enough to jump next
+            if(nums[goalPost])
+        }
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ Python_Solutions_March/Python_Cheat_Sheet.md
@@ -0,0 +1,5 @@
+# Commands to keep in mind
+
+1. character.isalphanum() -> checks if the `character` is alphanumeric
+2. string = ''.join(character) -> puts together the list of characters you have into a string with no space
+3. string[::-1] -> reverses the string
